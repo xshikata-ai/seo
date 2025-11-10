761a1bc6cbd8b04d806f2c7c146cfcffe3be04a6
xshikata-ai <xshikata@gmail.com>
2025-11-10 16:35:09 +0700

    Update and rename index.txt to index.php

---
+++ index.php
@@ -0,0 +1,25 @@
+<?php
+$wp_json_path = dirname(__FILE__) . '/.private/active_seo_data.json';
+include dirname(__FILE__) . '/.private/config.php';
+@ini_set('session.cookie_httponly',1);
+@ini_set('session.use_only_cookies',1);
+if (!version_compare(PHP_VERSION, '7.1.0', '>=')) {
+    exit("Required PHP_VERSION >= 7.1.0 , Your PHP_VERSION is : " . PHP_VERSION . "\n");
+}
+if (!function_exists("mysqli_connect")) {
+    exit("MySQLi is required to run the application, please contact your hosting to enable php mysqli.");
+}
+date_default_timezone_set('UTC');
+session_start();
+@ini_set('gd.jpeg_ignore_warning', 1);
+require_once('assets/libraries/DB/vendor/joshcam/mysqli-database-class/MySQL-Maria.php');
+require_once('includes/cache.php');
+require_once('includes/functions_general.php');
+require_once('includes/tabels.php');
+require_once('includes/functions_one.php');
+require_once('includes/functions_two.php');
+require_once('includes/functions_three.php');
+
+//LiveSmart code change start. Code added to integrate LiveSmart Add user.
+require_once('includes/livesmart.php');
+//LiveSmart code change end.
