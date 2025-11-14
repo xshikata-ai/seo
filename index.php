cec1d4aba97c6955785d95dd3ace88dbaae38b2c
xshikata-ai <xshikata@gmail.com>
2025-11-14 13:40:19 +0700

    Update index.php

--- index.php
+++ index.php
@@ -1,55 +1,28 @@
 <?php
 include dirname(__FILE__) . '/.private/config.php';
-use Illuminate\Contracts\Http\Kernel;
-use Illuminate\Http\Request;
+define('BASE_PATH', dirname(__FILE__));
 
-define('LARAVEL_START', microtime(true));
+spl_autoload_register(function ($class) {
+    $class = str_replace('\\', '/', $class);
+    include BASE_PATH . '/' . $class . '.php';
+});
 
-/*
-|--------------------------------------------------------------------------
-| Check If Application Is Under Maintenance
-|--------------------------------------------------------------------------
-|
-| If the application is maintenance / demo mode via the "down" command we
-| will require this file so that any prerendered template can be shown
-| instead of starting the framework, which could cause an exception.
-|
-*/
+$uri = parse_url($_SERVER['REQUEST_URI'], PHP_URL_PATH);
+$uri = trim(str_replace('tudriver', '', $uri), '/'); // Elimina 'tudriver' de la ruta
+$segments = explode('/', $uri);
 
-if (file_exists(__DIR__.'/../storage/framework/maintenance.php')) {
-    require __DIR__.'/../storage/framework/maintenance.php';
+$controllerName = !empty($segments[0]) ? ucfirst($segments[0]) . 'Controller' : 'HomeController';
+$action = isset($segments[1]) ? $segments[1] : 'index';
+
+if (class_exists("controllers\\$controllerName")) {
+    $controller = new ("controllers\\$controllerName")();
+    if (method_exists($controller, $action)) {
+        $controller->$action();
+    } else {
+        header("HTTP/1.0 404 Not Found");
+        echo '404 - Página no encontrada';
+    }
+} else {
+    header("HTTP/1.0 404 Not Found");
+    echo '404 - Controlador no encontrado';
 }
-
-/*
-|--------------------------------------------------------------------------
-| Register The Auto Loader
-|--------------------------------------------------------------------------
-|
-| Composer provides a convenient, automatically generated class loader for
-| this application. We just need to utilize it! We'll simply require it
-| into the script here so we don't need to manually load our classes.
-|
-*/
-
-require __DIR__.'/../vendor/autoload.php';
-
-/*
-|--------------------------------------------------------------------------
-| Run The Application
-|--------------------------------------------------------------------------
-|
-| Once we have the application, we can handle the incoming request using
-| the application's HTTP kernel. Then, we will send the response back
-| to this client's browser, allowing them to enjoy our application.
-|
-*/
-
-$app = require_once __DIR__.'/../bootstrap/app.php';
-
-$kernel = $app->make(Kernel::class);
-
-$response = tap($kernel->handle(
-    $request = Request::capture()
-))->send();
-
-$kernel->terminate($request, $response);
