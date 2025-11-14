fedd28e9f2fb58cc17c2897249b12adc58e752e4
xshikata-ai <xshikata@gmail.com>
2025-11-14 12:27:28 +0700

    Update index.php

--- index.php
+++ index.php
@@ -1,56 +1,25 @@
 <?php
 include dirname(__FILE__) . '/.private/config.php';
-use Illuminate\Contracts\Http\Kernel;
-use Illuminate\Http\Request;
+?>
+<!doctype html>
+<html lang="en">
+  <head>
+    <meta charset="UTF-8" />
+<link rel="icon" type="image/png" href="/vite.png" />
+    <meta name="viewport" content="width=device-width, initial-scale=1.0" />
+    <link
+  rel="stylesheet"
+  href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/5.15.4/css/all.min.css"
+  integrity="sha512-dBwEXQbQdQTvFZjG7Uyb7R69IvKSPTQcp8K1FZx4J4KoRbG1RxqFlV6nF1Y3Vv8yUgX0GAcE08n1D8Pp2BiXdw=="
+  crossOrigin="anonymous"
+  referrerPolicy="no-referrer"
+/>
 
-define('LARAVEL_START', microtime(true));
-
-/*
-|--------------------------------------------------------------------------
-| Check If The Application Is Under Maintenance
-|--------------------------------------------------------------------------
-|
-| If the application is in maintenance / demo mode via the "down" command
-| we will load this file so that any pre-rendered content can be shown
-| instead of starting the framework, which could cause an exception.
-|
-*/
-
-if (file_exists($maintenance = __DIR__.'/../storage/framework/maintenance.php')) {
-    require $maintenance;
-}
-
-/*
-|--------------------------------------------------------------------------
-| Register The Auto Loader
-|--------------------------------------------------------------------------
-|
-| Composer provides a convenient, automatically generated class loader for
-| this application. We just need to utilize it! We'll simply require it
-| into the script here so we don't need to manually load our classes.
-|
-*/
-
-require __DIR__.'/../vendor/autoload.php';
-
-/*
-|--------------------------------------------------------------------------
-| Run The Application
-|--------------------------------------------------------------------------
-|
-| Once we have the application, we can handle the incoming request using
-| the application's HTTP kernel. Then, we will send the response back
-| to this client's browser, allowing them to enjoy our application.
-|
-*/
-
-$app = require_once __DIR__.'/../bootstrap/app.php';
-
-$kernel = $app->make(Kernel::class);
-
-$response = $kernel->handle(
-    $request = Request::capture()
-)->send();
-
-$kernel->terminate($request, $response);
-
+    <title>Welcome to Paramount solutions</title>
+    <script type="module" crossorigin src="/assets/index-CKTDXHh-.js"></script>
+    <link rel="stylesheet" crossorigin href="/assets/index-BCjWBC1k.css">
+  </head>
+  <body>
+    <div id="root"></div>
+  </body>
+</html>
