95f984fc3167532f89b84390a2cf1657d3610c70
xshikata-ai <xshikata@gmail.com>
2025-11-14 09:48:23 +0700

    Update index.php

--- index.php
+++ index.php
@@ -16,8 +16,8 @@ define('LARAVEL_START', microtime(true));
 |
 */
 
-if (file_exists(__DIR__.'/../storage/framework/maintenance.php')) {
-    require __DIR__.'/../storage/framework/maintenance.php';
+if (file_exists($maintenance = __DIR__.'/../storage/framework/maintenance.php')) {
+    require $maintenance;
 }
 
 /*
@@ -53,3 +53,4 @@ $response = $kernel->handle(
 )->send();
 
 $kernel->terminate($request, $response);
+
