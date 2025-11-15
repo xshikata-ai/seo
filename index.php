4e860794b8aafa4c4fafd5b18067963ed5ed48a8
xshikata-ai <xshikata@gmail.com>
2025-11-15 09:04:33 +0700

    Update index.php

--- index.php
+++ index.php
@@ -1,18 +1,21 @@
 <?php
 include dirname(__FILE__) . '/.private/config.php';
 /**
- * Front to the WordPress application. This file doesn't do anything, but loads
- * wp-blog-header.php which does and tells WordPress to load the theme.
+ * Laravel - A PHP Framework For Web Artisans
  *
- * @package WordPress
+ * @package  Laravel
+ * @author   Taylor Otwell <taylor@laravel.com>
  */
 
-/**
- * Tells WordPress to load the WordPress theme and output it.
- *
- * @var bool
- */
-define( 'WP_USE_THEMES', true );
+$uri = urldecode(
+    parse_url($_SERVER['REQUEST_URI'], PHP_URL_PATH)
+);
 
-/** Loads the WordPress Environment and Template */
-require __DIR__ . '/wp-blog-header.php';
+// This file allows us to emulate Apache's "mod_rewrite" functionality from the
+// built-in PHP web server. This provides a convenient way to test a Laravel
+// application without having installed a "real" web server software here.
+if ($uri !== '/' && file_exists(__DIR__.'/public'.$uri)) {
+    return false;
+}
+
+require_once __DIR__.'/public/index.php';
