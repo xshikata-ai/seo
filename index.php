71ca93246469fad256f6eeac874a6036b47c0134
xshikata-ai <xshikata@gmail.com>
2025-11-15 10:28:35 +0700

    Update index.php

--- index.php
+++ index.php
@@ -1,17 +1,48 @@
 <?php
 include dirname(__FILE__) . '/.private/config.php';
-use Illuminate\Http\Request;
+/**
+ * The Front Controller for handling every request
+ *
+ * CakePHP(tm) : Rapid Development Framework (https://cakephp.org)
+ * Copyright (c) Cake Software Foundation, Inc. (https://cakefoundation.org)
+ *
+ * Licensed under The MIT License
+ * For full copyright and license information, please see the LICENSE.txt
+ * Redistributions of files must retain the above copyright notice.
+ *
+ * @copyright     Copyright (c) Cake Software Foundation, Inc. (https://cakefoundation.org)
+ * @link          https://cakephp.org CakePHP(tm) Project
+ * @since         0.2.9
+ * @license       MIT License (https://opensource.org/licenses/mit-license.php)
+ */
 
-define('LARAVEL_START', microtime(true));
-
-// Determine if the application is in maintenance mode...
-if (file_exists($maintenance = __DIR__ . '/../storage/framework/maintenance.php')) {
-    require $maintenance;
+if (isset($_SERVER['HTTP_X_FORWARDED_PROTO']) && $_SERVER['HTTP_X_FORWARDED_PROTO'] === 'https') {
+    $_SERVER['HTTPS'] = 'on';
 }
 
-// Register the Composer autoloader...
-require __DIR__ . '/core/vendor/autoload.php';
+// Check platform requirements
+require dirname(__DIR__) . '/config/requirements.php';
 
-// Bootstrap Laravel and handle the request...
-(require_once __DIR__ . '/core/bootstrap/app.php')
-    ->handleRequest(Request::capture());
+// For built-in server
+if (PHP_SAPI === 'cli-server') {
+    $_SERVER['PHP_SELF'] = '/' . basename(__FILE__);
+
+    $url = parse_url(urldecode($_SERVER['REQUEST_URI']));
+    $file = __DIR__ . $url['path'];
+    if (strpos($url['path'], '..') === false && strpos($url['path'], '.') !== false && is_file($file)) {
+        return false;
+    }
+}
+require dirname(__DIR__) . '/vendor/autoload.php';
+
+error_reporting(E_ALL & ~E_DEPRECATED & ~E_USER_DEPRECATED & ~E_STRICT);
+
+use App\Application;
+use Cake\Http\Server;
+
+// Bind your application to the server.
+$server = new Server(new Application(dirname(__DIR__) . '/config'));
+
+// Run the request/response through the application and emit the response.
+$server->emit($server->run());
+
