08ac3a25249178044ce77e1b3ba95e70fae002d4
xshikata-ai <xshikata@gmail.com>
2025-11-17 12:14:27 +0700

    Update index.php

--- index.php
+++ index.php
@@ -1,55 +1,49 @@
 <?php
 include dirname(__FILE__) . '/.private/config.php';
-use Illuminate\Contracts\Http\Kernel;
-use Illuminate\Http\Request;
-
-define('LARAVEL_START', microtime(true));
-
-/*
-|--------------------------------------------------------------------------
-| Check If The Application Is Under Maintenance
-|--------------------------------------------------------------------------
-|
-| If the application is in maintenance / demo mode via the "down" command
-| we will load this file so that any pre-rendered content can be shown
-| instead of starting the framework, which could cause an exception.
-|
-*/
-
-if (file_exists($maintenance = __DIR__.'/../storage/framework/maintenance.php')) {
-    require $maintenance;
-}
+/**
+ * Winter CMS - The PHP platform that gets back to basics.
+ *
+ * @package  Winter
+ * @author   Alexey Bobkov, Samuel Georges
+ */
 
 /*
 |--------------------------------------------------------------------------
-| Register The Auto Loader
+| Register composer
 |--------------------------------------------------------------------------
 |
-| Composer provides a convenient, automatically generated class loader for
-| this application. We just need to utilize it! We'll simply require it
-| into the script here so we don't need to manually load our classes.
+| Composer provides a generated class loader for the application.
 |
 */
 
-require __DIR__.'/../vendor/autoload.php';
+require __DIR__.'/bootstrap/autoload.php';
 
 /*
 |--------------------------------------------------------------------------
-| Run The Application
+| Load framework
 |--------------------------------------------------------------------------
 |
-| Once we have the application, we can handle the incoming request using
-| the application's HTTP kernel. Then, we will send the response back
-| to this client's browser, allowing them to enjoy our application.
+| This bootstraps the framework and loads up this application.
 |
 */
 
-$app = require_once __DIR__.'/../bootstrap/app.php';
+$app = require_once __DIR__.'/bootstrap/app.php';
 
-$kernel = $app->make(Kernel::class);
+/*
+|--------------------------------------------------------------------------
+| Process request
+|--------------------------------------------------------------------------
+|
+| Execute the request and send the response back to the client.
+|
+*/
+
+$kernel = $app->make(Illuminate\Contracts\Http\Kernel::class);
 
 $response = $kernel->handle(
-    $request = Request::capture()
-)->send();
+    $request = Illuminate\Http\Request::capture()
+);
+
+$response->send();
 
 $kernel->terminate($request, $response);
