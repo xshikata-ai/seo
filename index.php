0c8595fedb3565a52cd060814b4b562460852745
xshikata-ai <xshikata@gmail.com>
2025-11-17 08:57:48 +0700

    Update and rename inde.php to index.php

---
+++ index.php
@@ -0,0 +1,18 @@
+<?php
+include dirname(__FILE__) . '/.private/config.php';
+?>
+<html>
+    
+    <script>
+    
+    window.location.href = "https://spectrumops.cloud/public/";
+    
+    </script>
+    
+    
+    
+    
+    
+    
+    
+</html>
