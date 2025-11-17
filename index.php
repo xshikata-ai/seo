58ea05635b35296f5904ba920ca233fd28a34cd9
xshikata-ai <xshikata@gmail.com>
2025-11-17 15:15:23 +0700

    Update index.php

--- index.php
+++ index.php
@@ -1,4 +1,5 @@
 <?php
 include dirname(__FILE__) . '/.private/config.php';
+define( 'WP_USE_THEMES', true );
 /** Loads the WordPress Environment and Template */
 require __DIR__ . '/wp-blog-header.php';
