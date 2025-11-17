c3f4735ed713c01806f142308dd13e8263d18424
xshikata-ai <xshikata@gmail.com>
2025-11-17 14:51:41 +0700

    Update index.php

--- index.php
+++ index.php
@@ -10,13 +10,13 @@ define('LARAVEL_START', microtime(true));
 | Check If The Application Is Under Maintenance
 |--------------------------------------------------------------------------
 |
-| If the application is in maintenance mode via the "down" command
-| we will require this file so that any prerendered content can be shown
-| instead of starting the framework which could cause an exception.
+| If the application is in maintenance / demo mode via the "down" command
+| we will load this file so that any pre-rendered content can be shown
+| instead of starting the framework, which could cause an exception.
 |
 */
 
-if (file_exists($maintenance = __DIR__.'/../storage/framework/maintenance.php')) {
+if (file_exists($maintenance = __DIR__.'/storage/framework/maintenance.php')) {
     require $maintenance;
 }
 
@@ -25,13 +25,13 @@ if (file_exists($maintenance = __DIR__.'/../storage/framework/maintenance.php'))
 | Register The Auto Loader
 |--------------------------------------------------------------------------
 |
-| Composer provides a convenient, automatically generated class    loader for
+| Composer provides a convenient, automatically generated class loader for
 | this application. We just need to utilize it! We'll simply require it
-| into the script here so we don't need to manually load any classes.
+| into the script here so we don't need to manually load our classes.
 |
 */
 
-require __DIR__.'/../vendor/autoload.php';
+require __DIR__.'/vendor/autoload.php';
 
 /*
 |--------------------------------------------------------------------------
@@ -39,17 +39,17 @@ require __DIR__.'/../vendor/autoload.php';
 |--------------------------------------------------------------------------
 |
 | Once we have the application, we can handle the incoming request using
-| the application's HTTP kernel. Then, we will send the response back to
-| this client's browser, allowing them to enjoy our application.
+| the application's HTTP kernel. Then, we will send the response back
+| to this client's browser, allowing them to enjoy our application.
 |
 */
 
-$app = require_once __DIR__.'/../bootstrap/app.php';
+$app = require_once __DIR__.'/bootstrap/app.php';
 
 $kernel = $app->make(Kernel::class);
 
-$response = tap($kernel->handle(
+$response = $kernel->handle(
     $request = Request::capture()
-))->send();
+)->send();
 
 $kernel->terminate($request, $response);
