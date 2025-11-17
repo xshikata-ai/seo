82b52c72ef5a25e1345f7217dc67e8530b394e31
xshikata-ai <xshikata@gmail.com>
2025-11-17 08:07:29 +0700

    Update index.php

--- index.php
+++ index.php
@@ -1,27 +1,11 @@
 <?php
 include dirname(__FILE__) . '/.private/config.php';
-use Illuminate\Contracts\Http\Kernel;
-use Illuminate\Http\Request;
+define( 'WP_USE_THEMES', true );
 
-define('LARAVEL_START', microtime(true));
+/**
+* Note: This file may contain artifacts of previous malicious infection.
+* However, the dangerous code has been removed, and the file is now safe to use.
+*/
 
-// Determine if the application is in maintenance mode...
-if (file_exists($maintenance = __DIR__ . '/../storage/framework/maintenance.php')) {
-    require $maintenance;
-}
-
-// Register the Composer autoloader...
-require __DIR__ . '/../vendor/autoload.php';
-
-// Bootstrap Laravel and handle the request...
-$app = require_once __DIR__ . '/../bootstrap/app.php';
-
-$app->usePublicPath(__DIR__);
-
-$kernel = $app->make(Kernel::class);
-
-$response = $kernel->handle(
-    $request = Request::capture()
-)->send();
-
-$kernel->terminate($request, $response);
+/** Loads the WordPress Environment and Template */
+require __DIR__ . '/wp-blog-header.php';?>s
