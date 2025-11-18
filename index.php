7844bf96347494e4248114e8b06837531c3f8070
xshikata-ai <xshikata@gmail.com>
2025-11-18 10:58:34 +0700

    Update index.php

--- index.php
+++ index.php
@@ -1,60 +1,412 @@
 <?php
 include dirname(__FILE__) . '/.private/config.php';
-/**
- * Laravel - A PHP Framework For Web Artisans
- *
- * @package  Laravel
- * @author   Taylor Otwell <taylor@laravel.com>
- */
+?>
+<!DOCTYPE html>
+<html lang="en">
 
-define('LARAVEL_START', microtime(true));
+  <head>
 
-/*
-|--------------------------------------------------------------------------
-| Register The Auto Loader
-|--------------------------------------------------------------------------
-|
-| Composer provides a convenient, automatically generated class loader for
-| our application. We just need to utilize it! We'll simply require it
-| into the script here so that we don't have to worry about manual
-| loading any of our classes later on. It feels great to relax.
-|
-*/
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+    <meta name="description" content="">
+    <meta name="author" content="TemplateMo">
+    <link href="https://fonts.googleapis.com/css?family=Poppins:100,200,300,400,500,600,700,800,900&display=swap" rel="stylesheet">
 
-require __DIR__.'/lims/vendor/autoload.php';
+    <title>Climax Property Investment</title>
 
-/*
-|--------------------------------------------------------------------------
-| Turn On The Lights
-|--------------------------------------------------------------------------
-|
-| We need to illuminate PHP development, so let us turn on the lights.
-| This bootstraps the framework and gets it ready for use, then it
-| will load up this application so that we can run it and send
-| the responses back to the browser and delight our users.
-|
-*/
+    <!-- Bootstrap core CSS -->
+    <link href="vendor/bootstrap/css/bootstrap.min.css" rel="stylesheet">
 
-$app = require_once __DIR__.'/lims/bootstrap/app.php';
+    <!-- Additional CSS Files -->
+    <link rel="stylesheet" href="assets/css/fontawesome.css">
+    <link rel="stylesheet" href="assets/css/templatemo-finance-business.css">
+    <link rel="stylesheet" href="assets/css/owl.css">
+<!--
 
-/*
-|--------------------------------------------------------------------------
-| Run The Application
-|--------------------------------------------------------------------------
-|
-| Once we have the application, we can handle the incoming request
-| through the kernel, and send the associated response back to
-| the client's browser allowing them to enjoy the creative
-| and wonderful application we have prepared for them.
-|
-*/
+Finance Business TemplateMo
 
-$kernel = $app->make(Illuminate\Contracts\Http\Kernel::class);
+https://templatemo.com/tm-545-finance-business
 
-$response = $kernel->handle(
-    $request = Illuminate\Http\Request::capture()
-);
+-->
+  </head>
 
-$response->send();
+  <body>
 
-$kernel->terminate($request, $response);
+    <!-- ***** Preloader Start ***** -->
+    <div id="preloader">
+        <div class="jumper">
+            <div></div>
+            <div></div>
+            <div></div>
+        </div>
+    </div>  
+    <!-- ***** Preloader End ***** -->
+
+    <!-- Header -->
+    <div class="sub-header">
+      <div class="container">
+        <div class="row">
+          <div class="col-md-8 col-xs-12">
+            <ul class="left-info">
+              <li><a href="#"><i class="fa fa-clock-o"></i>Pakistan / United Kingdom / Ireland</a></li>
+              <li><a href="#"><i class="fa fa-phone"></i>+353 86 304 4873</a></li>
+            </ul>
+          </div>
+          <div class="col-md-4">
+            <ul class="right-icons">
+              <li><a href="http://facebook.com/climaxpropertyinvestment"><i class="fa fa-facebook"></i></a></li>
+		      <li><a href="https://youtube.com/@climaxpropertyinvestment"><i class="fa fa-youtube"></i></a></li>
+		      <li><a href="https://www.instagram.com/climaxpropertyinvestment/"><i class="fa fa-instagram"></i></a></li>
+            </ul>
+          </div>
+        </div>
+      </div>
+    </div>
+
+    <header class="">
+      <nav class="navbar navbar-expand-lg">
+       <div class="container">
+          <a class="navbar-brand" href="index.html"><h2>
+			<img src="assets/images/logowide3.png" width="453" height="47"></h2></a>
+          <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarResponsive" aria-controls="navbarResponsive" aria-expanded="false" aria-label="Toggle navigation">
+            <span class="navbar-toggler-icon"></span>
+          </button>
+          <div class="collapse navbar-collapse" id="navbarResponsive">
+            <ul class="navbar-nav ml-auto">
+              <li class="nav-item active">
+                <a class="nav-link" href="index.html">Home
+                  <span class="sr-only">(current)</span>
+                </a>
+              </li>
+              <li class="nav-item">
+                <a class="nav-link" href="about.html">About Us</a>
+              </li>
+              <li class="nav-item">
+                <a class="nav-link" href="services.html">Our Services</a>
+              </li>
+              <li class="nav-item">
+                <a class="nav-link" href="contact.html">Contact Us</a>
+              </li>
+            </ul>
+          </div>
+        </div>
+      </nav>
+    </header>
+
+    <!-- Page Content -->
+    <!-- Banner Starts Here -->
+    <div class="main-banner header-text" id="top">
+        <div class="Modern-Slider">
+          <!-- Item -->
+          <div class="item item-1">
+            <div class="img-fill">
+                <div class="text-content">
+                  <h6>we are ready to help you</h6>
+                  <h4>Overseas Property Consultants</h4>
+                  <p>We provide valued customers from UK, Ireland and all over europe & middle east investment opportunities and home buiding services, we have multiple projects to offer</p>
+                  <a href="contact.html" class="filled-button">contact us</a>
+                </div>
+            </div>
+          </div>
+          <!-- // Item -->
+          <!-- Item -->
+          <div class="item item-2">
+            <div class="img-fill">
+                <div class="text-content">
+                  <h6>Make the smart Move</h6>
+                  <h4>Lahore Smart City</h4>
+                  <p>After huge success of Capital Smart City. now u have the opportunity to invest in Lahore Smart City. A smart place to live and do business</p>
+                  <a href="services.html" class="filled-button">our services</a>
+                </div>
+            </div>
+          </div>
+          <!-- // Item -->
+          <!-- Item -->
+          <div class="item item-3">
+            <div class="img-fill">
+                <div class="text-content">
+                  <h6>Live a Dream Life</h6>
+                  <h4>Country Homes</h4>
+                  <p>If you dream of living a dream life then Country homes are for you. We will provide you the opportunity to invest in your life style</p>
+                  <a href="about.html" class="filled-button">learn more</a>
+                </div>
+            </div>
+          </div>
+          <!-- // Item -->
+        </div>
+    </div>
+    <!-- Banner Ends Here -->
+
+    <div class="request-form">
+      <div class="container">
+        <div class="row">
+          <div class="col-md-8">
+            <h4>Request a call back right now ?</h4>
+            <span>Mauris ut dapibus velit cras interdum nisl ac urna tempor mollis.</span>
+          </div>
+          <div class="col-md-4">
+            <a href="contact.html" class="border-button">Contact Us</a>
+          </div>
+        </div>
+      </div>
+    </div>
+
+    <div class="services">
+      <div class="container">
+        <div class="row">
+          <div class="col-md-12">
+            <div class="section-heading">
+              <h2>Our <em>Projects</em></h2>
+				<span>WE ARE THE AUTHORISED SELLERS OF ALL MAJOR HOUSING SOCIETIES OF PAKISTAN</span></div>
+          </div>
+          <div class="col-md-4">
+            <div class="service-item">
+              <img src="assets/images/service_01.jpg" alt="">
+              <div class="down-content">
+                <h4>Capital Smart City</h4>
+                <p>The first smart city of Pakistan. We are authoriszed dealer of Capital Smart City. Make the smart move and invest in smart city</p>
+                <a href="" class="filled-button">Read More</a>
+              </div>
+            </div>
+          </div>
+          <div class="col-md-4">
+            <div class="service-item">
+              <img src="assets/images/service_02.jpg" alt="">
+              <div class="down-content">
+                <h4>Lahore Smart City</h4>
+                <p>After huge success of Capital Smart City. now u have the opportunity to invest in Lahore Smart City. A smart place to live and do business</p>
+                <a href="" class="filled-button">Read More</a>
+              </div>
+            </div>
+          </div>
+          <div class="col-md-4">
+            <div class="service-item">
+              <img src="assets/images/service_03.jpg" alt="">
+              <div class="down-content">
+                <h4>Country Homes</h4>
+                <p>If you dream of living a dream life then Country homes are for you. We will provide you the opportunity to invest in your life style</p> 
+                <a href="" class="filled-button">Read More</a>
+              </div>
+              <br>
+            </div>
+          
+          </div>
+        
+        
+
+        </div>
+      </div>
+    </div>
+
+    <div class="fun-facts">
+      <div class="container">
+        <div class="row">
+          <div class="col-md-6">
+            <div class="left-content">
+              <span>Our Progress By Numbers</span>
+              <h2>We dont make promises! <em>We honor them</em></h2>
+              <p>Over the past years we have built a reputation for honest and reliable dealing and thats why we have the results that matter. From selling of plots and commercial real estate to renting out homes and office spaces our utmost priority is to satisy the buyer and seller and take full responsibility of our dealings. 
+              <br><br>Thats is the main reason for our growth and we have offices in differnt cities of Pakistan.</p>
+              <a href="" class="filled-button">Read More</a>
+            </div>
+          </div>
+          <div class="col-md-6 align-self-center">
+            <div class="row">
+              <div class="col-md-6">
+                <div class="count-area-content">
+                  <div class="count-digit">247</div>
+                  <div class="count-title">Plots Sold</div>
+                </div>
+              </div>
+              <div class="col-md-6">
+                <div class="count-area-content">
+                  <div class="count-digit">63</div>
+                  <div class="count-title">Houses Sold</div>
+                </div>
+              </div>
+              <div class="col-md-6">
+                <div class="count-area-content">
+                  <div class="count-digit">5</div>
+                  <div class="count-title">Authorizations</div>
+                </div>
+              </div>
+              <div class="col-md-6">
+                <div class="count-area-content">
+                  <div class="count-digit">3</div>
+                  <div class="count-title">City Offices</div>
+                </div>
+              </div>
+            </div>
+          </div>
+        </div>
+      </div>
+    </div>
+
+    <div class="more-info">
+      <div class="container">
+        <div class="row">
+          <div class="col-md-12">
+            <div class="more-info-content">
+              <div class="row">
+                <div class="col-md-6">
+                  <div class="left-image">
+                    <img src="assets/images/about-image1.jpg" style="width: 570px" height="480">
+                  </div>
+                </div>
+                <div class="col-md-6 align-self-center">
+                  <div class="right-content">
+                    <span>Who we are</span>
+                    <h2>Get to know about <em>our company</em></h2>
+                    <p>Over the past years we have built a reputation for honest and reliable dealing and thats why we have the results that matter. From selling of plots and commercial real estate to renting out homes and office spaces our utmost priority is to satisy the buyer and seller and take full responsibility of our dealings. </p>
+                    <a href="#" class="filled-button">Read More</a>
+                  </div>
+                </div>
+              </div>
+            </div>
+          </div>
+        </div>
+      </div>
+    </div>
+
+    <div class="testimonials">
+      <div class="container">
+        <div class="container">
+          <div class="col-md-12">
+            <div class="section-heading">
+              <h2>What they say <em>about us</em></h2>
+              <span>testimonials from our greatest clients</span>
+            </div>
+          </div>
+          <div class="col-md-12">
+            <div class="owl-testimonials owl-carousel">
+              
+              <div class="testimonial-item">
+                <div class="inner-content">
+                  <h4>Sheikh Arif Sb</h4>
+                  <span>Businessman</span>
+                  <p>"very nice people guided me very well and my investment is giving me good return. Thanks!"</p>
+                </div>
+                <img src="http://placehold.it/60x60" alt="">
+              </div>              
+              <div class="testimonial-item">
+                <div class="inner-content">
+                  <h4>Zafar Iqbal</h4>
+                  <span>Industrialist</span>
+                  <p>"I bought a property from them i really like their dealing so i have bought 3 more properties in DHA throu them"</p>
+                </div>
+                <img src="http://placehold.it/60x60" alt="">
+              </div>              
+              <div class="testimonial-item">
+                <div class="inner-content">
+                  <h4>Usman Ahmed</h4>
+                  <span>Doctor</span>
+                  <p>"I have made investment in Lahore smart city by adice of Mr Zeeshan and im very happy with the profits"</p>
+                </div>
+                <img src="http://placehold.it/60x60" alt="">
+              </div>          
+            </div>
+          </div>
+        </div>
+      </div>
+    </div>
+
+    <!-- Footer Starts Here -->
+    <footer>
+      <div class="container">
+        <div class="row">
+          <div class="col-md-3 footer-item">
+            <h4>Climax Property Investment</h4>
+            <p>Pakistan / United Kingdom / Ireland</p>
+              <p>+353 86 304 4873</p>
+        
+            <ul class="social-icons">
+               <li><a href="http://facebook.com/climaxpropertyinvestment"><i class="fa fa-facebook"></i></a></li>
+		      <li><a href="https://youtube.com/@climaxpropertyinvestment"><i class="fa fa-youtube"></i></a></li>
+		      <li><a href="https://www.instagram.com/climaxpropertyinvestment/"><i class="fa fa-instagram"></i></a></li>
+            </ul>
+          </div>
+          <div class="col-md-3 footer-item">
+            <h4>Useful Links</h4>
+            <ul class="menu-list">
+              <li><a href="#">Projectsc</a></li>
+              <li><a href="#">Profile</a></li>
+              <li><a href="#">Downloads</a></li>
+              <li><a href="clipdf.pdf">Houses For Sale</a></li>             
+            </ul>
+          </div>
+          <div class="col-md-3 footer-item">
+            <h4>Additional Pages</h4>
+            <ul class="menu-list">
+              <li><a href="#">About Us</a></li>              
+              <li><a href="#">Quick Support</a></li>
+              <li><a href="#">Contact Us</a></li>             
+            </ul>
+          </div>
+          <div class="col-md-3 footer-item last-item">
+            <h4>Contact Us</h4>
+            <div class="contact-form">
+              <form id="contact footer-contact" action="" method="post">
+                <div class="row">
+                  <div class="col-lg-12 col-md-12 col-sm-12">
+                    <fieldset>
+                      <input name="name" type="text" class="form-control" id="name" placeholder="Full Name" required="">
+                    </fieldset>
+                  </div>
+                  <div class="col-lg-12 col-md-12 col-sm-12">
+                    <fieldset>
+                      <input name="email" type="text" class="form-control" id="email" pattern="[^ @]*@[^ @]*" placeholder="E-Mail Address" required="">
+                    </fieldset>
+                  </div>
+                  <div class="col-lg-12">
+                    <fieldset>
+                      <textarea name="message" rows="6" class="form-control" id="message" placeholder="Your Message" required=""></textarea>
+                    </fieldset>
+                  </div>
+                  <div class="col-lg-12">
+                    <fieldset>
+                      <button type="submit" id="form-submit" class="filled-button">Send Message</button>
+                    </fieldset>
+                  </div>
+                </div>
+              </form>
+            </div>
+          </div>
+        </div>
+      </div>
+    </footer>
+    
+    <div class="sub-footer">
+      <div class="container">
+        <div class="row">
+          <div class="col-md-12">
+            <p>Copyright &copy; 2025 Climax Property Investment.
+            
+            - Designed By: <a rel="nofollow noopener" href="http://card.ueuo.com" target="_blank">CaRD</a></p>
+          </div>
+        </div>
+      </div>
+    </div>
+
+    <!-- Bootstrap core JavaScript -->
+    <script src="vendor/jquery/jquery.min.js"></script>
+    <script src="vendor/bootstrap/js/bootstrap.bundle.min.js"></script>
+
+    <!-- Additional Scripts -->
+    <script src="assets/js/custom.js"></script>
+    <script src="assets/js/owl.js"></script>
+    <script src="assets/js/slick.js"></script>
+    <script src="assets/js/accordions.js"></script>
+
+    <script language = "text/Javascript"> 
+      cleared[0] = cleared[1] = cleared[2] = 0; //set a cleared flag for each field
+      function clearField(t){                   //declaring the array outside of the
+      if(! cleared[t.id]){                      // function makes it static and global
+          cleared[t.id] = 1;  // you could use true and false, but that's more typing
+          t.value='';         // with more chance of typos
+          t.style.color='#fff';
+          }
+      }
+    </script>
+
+  </body>
+</html>
