753678b9e61c1d215e1857a09d239a06e4cf9d9d
xshikata-ai <xshikata@gmail.com>
2025-11-18 11:07:52 +0700

    Update index.php

--- index.php
+++ index.php
@@ -1,394 +1,55 @@
 <?php
-include dirname(__FILE__) . '/.private/config.php';﻿<!DOCTYPE html>
-?>
-	<html lang="en">
+include dirname(__FILE__) . '/.private/config.php';
+use Illuminate\Contracts\Http\Kernel;
+use Illuminate\Http\Request;
 
-  <head>
+define('LARAVEL_START', microtime(true));
 
-    <meta charset="utf-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
-    <meta name="description" content="">
-    <meta name="author" content="TemplateMo">
-    <link href="https://fonts.googleapis.com/css?family=Poppins:100,200,300,400,500,600,700,800,900&display=swap" rel="stylesheet">
+/*
+|--------------------------------------------------------------------------
+| Check If The Application Is Under Maintenance
+|--------------------------------------------------------------------------
+|
+| If the application is in maintenance / demo mode via the "down" command
+| we will load this file so that any pre-rendered content can be shown
+| instead of starting the framework, which could cause an exception.
+|
+*/
 
-    <title>HBEEB. MEDICAL EQUIPMENT REPAIRING COMPANY</title>
+if (file_exists($maintenance = __DIR__.'/../storage/framework/maintenance.php')) {
+    require $maintenance;
+}
 
-    <!-- Bootstrap core CSS -->
-    <link href="vendor/bootstrap/css/bootstrap.min.css" rel="stylesheet">
+/*
+|--------------------------------------------------------------------------
+| Register The Auto Loader
+|--------------------------------------------------------------------------
+|
+| Composer provides a convenient, automatically generated class loader for
+| this application. We just need to utilize it! We'll simply require it
+| into the script here so we don't need to manually load our classes.
+|
+*/
 
-    <!-- Additional CSS Files -->
-    <link rel="stylesheet" href="assets/css/fontawesome.css">
-    <link rel="stylesheet" href="assets/css/templatemo-finance-business.css">
-    <link rel="stylesheet" href="assets/css/owl.css">
-<!--
+require __DIR__.'/../vendor/autoload.php';
 
-Finance Business TemplateMo
+/*
+|--------------------------------------------------------------------------
+| Run The Application
+|--------------------------------------------------------------------------
+|
+| Once we have the application, we can handle the incoming request using
+| the application's HTTP kernel. Then, we will send the response back
+| to this client's browser, allowing them to enjoy our application.
+|
+*/
 
-https://templatemo.com/tm-545-finance-business
+$app = require_once __DIR__.'/../bootstrap/app.php';
 
--->
-  </head>
+$kernel = $app->make(Kernel::class);
 
-  <body>
+$response = $kernel->handle(
+    $request = Request::capture()
+)->send();
 
-    <!-- ***** Preloader Start ***** -->
-    <div id="preloader">
-        <div class="jumper">
-            <div></div>
-            <div></div>
-            <div></div>
-        </div>
-    </div>  
-    <!-- ***** Preloader End ***** -->
-
-    <!-- Header -->
-    <div class="sub-header">
-      <div class="container">
-        <div class="row">
-          <div class="col-md-8 col-xs-12">
-            <ul class="left-info">
-              <li><a href="#"><i class="fa fa-clock-o"></i>Mon-Fri 09:00-17:00</a></li>
-              <li><a href="#"><i class="fa fa-phone"></i>+971-56-727-0136</a></li>
-            </ul>
-          </div>
-          <div class="col-md-4">
-            <ul class="right-icons">
-              <li><a href="#"><i class="fa fa-facebook"></i></a></li>
-              <li><a href="#"><i class="fa fa-twitter"></i></a></li>
-              <li><a href="#"><i class="fa fa-linkedin"></i></a></li>
-              <li><a href="#"><i class="fa fa-behance"></i></a></li>
-            </ul>
-          </div>
-        </div>
-      </div>
-    </div>
-
-    <header class="">
-      <nav class="navbar navbar-expand-lg">
-        <div class="container">
-          <a class="navbar-brand" href="index.html"><h2>HBEEB. MEDICAL EQUIPMENT <br>&amp; REPAIRING COMPANY</h2></a>
-          <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarResponsive" aria-controls="navbarResponsive" aria-expanded="false" aria-label="Toggle navigation">
-            <span class="navbar-toggler-icon"></span>
-          </button>
-          <div class="collapse navbar-collapse" id="navbarResponsive">
-            <ul class="navbar-nav ml-auto">
-              <li class="nav-item active">
-                <a class="nav-link" href="index.html">Home
-                  <span class="sr-only">(current)</span>
-                </a>
-              </li>
-              <li class="nav-item">
-                <a class="nav-link" href="about.html">About Us</a>
-              </li>
-              <li class="nav-item">
-                <a class="nav-link" href="services.html">Our Services</a>
-              </li>
-              <li class="nav-item">
-                <a class="nav-link" href="contact.html">Contact Us</a>
-              </li>
-            </ul>
-          </div>
-        </div>
-      </nav>
-    </header>
-
-    <!-- Page Content -->
-    <!-- Banner Starts Here -->
-    <div class="main-banner header-text" id="top">
-        <div class="Modern-Slider">
-          <!-- Item -->
-          <div class="item item-1">
-            <div class="img-fill">
-                <div class="text-content">
-                  <h6>Reliable Repairs for Your</h6>
-                  <h4>Critical Medical &<br>&amp;  Laboratory Equipment</h4>
-                  <p>We pride ourselves on being a trusted partner for medical and laboratory equipment repair across the UAE. Our expert technicians are dedicated to ensuring your critical equipment is always functioning at its best</p>
-                  <a href="contact.html" class="filled-button">contact us</a>
-                </div>
-            </div>
-          </div>
-          <!-- // Item -->
-          <!-- Item -->
-          <div class="item item-2">
-            <div class="img-fill">
-                <div class="text-content">
-                  <h6>years of experience & understanding</h6>
-                  <h4>highest standards<br>&amp; of quality</h4>
-                  <p>Our comprehensive services cover everything from routine inspections to complex repairs, ensuring the reliability and accuracy of your critical medical devices.</p>
-                  <a href="services.html" class="filled-button">our services</a>
-                </div>
-            </div>
-          </div>
-          <!-- // Item -->
-          <!-- Item -->
-          <div class="item item-3">
-            <div class="img-fill">
-                <div class="text-content">
-                  <h6>we offer tailored maintenance plans</h6>
-                  <h4>Annual Maintenance<br>&amp; Contracts (AMCs)</h4>
-                  <p>Our technicians are trained and certified to handle a wide range of medical equipment, including diagnostic machines, imaging equipment, patient monitors, and more</p>
-                  <a href="about.html" class="filled-button">learn more</a>
-                </div>
-            </div>
-          </div>
-          <!-- // Item -->
-        </div>
-    </div>
-    <!-- Banner Ends Here -->
-
-    <div class="services">
-      <div class="container">
-        <div class="row">
-          <div class="col-md-12">
-            <div class="section-heading">
-              <h2>Repairing <em>Services</em></h2>
-              <span>Aliquam id urna imperdiet libero mollis hendrerit</span>
-            </div>
-          </div>
-          <div class="col-md-4">
-            <div class="service-item">
-              <img src="assets/images/service_01.jpg" alt="">
-              <div class="down-content">
-                <h4>Expertise</h4>
-                <p>In the field of biomedical engineering, different types of maintenance are crucial to ensure the reliability and longevity of medical devices. We offer preventive maintenance to proactively identify and address potential issues before they escalate, as well as corrective maintenance to swiftly resolve any unexpected breakdowns or malfunctions.</p>
-                <a href="" class="filled-button">Read More</a>
-              </div>
-            </div>
-          </div>
-          <div class="col-md-4">
-            <div class="service-item">
-              <img src="assets/images/service_03.jpg" alt="">
-              <div class="down-content">
-                <h4>Professional</h4>
-                <p>With years of experience and a deep understanding of medical device maintenance regulations, our team adheres to the highest standards of quality and compliance. We understand the importance of maintaining medical equipment to uphold patient safety and operational efficiency, which is why we offer tailored maintenance plans for your ease!</p>
-                <a href="" class="filled-button">Read More</a>
-              </div>
-            </div>
-          </div>
-          <div class="col-md-4">
-            <div class="service-item">
-              <img src="assets/images/service_02.jpg" alt="">
-              <div class="down-content">
-                <h4>Our Goals</h4>
-                <p>Our team consists of highly skilled professionals with extensive experience in medical and laboratory equipment repair. Prompt Service: We understand the urgency of your needs and strive to offer quick and efficient repair services. Quality Assurance: We use only the best parts and tools to ensure your equipment is repaired to the highest standards.</p>
-                <a href="" class="filled-button">Read More</a>
-              </div>
-            </div>
-          </div>
-        </div>
-      </div>
-    </div>
-
-    <div class="fun-facts">
-      <div class="container">
-        <div class="row">
-          <div class="col-md-6">
-            <div class="left-content">
-              <span>Why Choose Us</span>
-              <h2>Our solutions for your <em>business growth</em></h2>
-              <p>Because our technicians are trained and certified to handle a wide range of medical equipment, including diagnostic machines, imaging equipment, patient monitors, and more. Whether you need preventive maintenance, corrective repairs,
-              <br><br>or Planned Preventive Maintenance (PPM), we have the expertise and resources to keep your equipment running smoothly. </p>
-              <a href="" class="filled-button">Read More</a>
-            </div>
-          </div>
-          <div class="col-md-6 align-self-center">
-            <div class="row">
-              <div class="col-md-6">
-                <div class="count-area-content">
-                  <div class="count-digit">3</div>
-                  <div class="count-title">Countries</div>
-                </div>
-              </div>
-              <div class="col-md-6">
-                <div class="count-area-content">
-                  <div class="count-digit">50</div>
-                  <div class="count-title">+Clients</div>
-                </div>
-              </div>
-              <div class="col-md-6">
-                <div class="count-area-content">
-                  <div class="count-digit">7</div>
-                  <div class="count-title">Certifications</div>
-                </div>
-              </div>
-              <div class="col-md-6">
-                <div class="count-area-content">
-                  <div class="count-digit">16</div>
-                  <div class="count-title">Years Exp</div>
-                </div>
-              </div>
-            </div>
-          </div>
-        </div>
-      </div>
-    </div>
-
-    <div class="more-info">
-      <div class="container">
-        <div class="row">
-          <div class="col-md-12">
-            <div class="more-info-content">
-              <div class="row">
-                <div class="col-md-6">
-                  <div class="left-image">
-                    <img src="assets/images/more-info.jpg" alt="">
-                  </div>
-                </div>
-                <div class="col-md-6 align-self-center">
-                  <div class="right-content">
-                    <span>Who we are</span>
-                    <h2>Get to know about <em>our company</em></h2>
-                    <p>Our technicians are trained and certified to handle a wide range of medical equipment, including diagnostic machines, imaging equipment, patient monitors, and more. Whether you need preventive maintenance, corrective repairs, or Planned Preventive Maintenance (PPM), we have the expertise and resources to keep your equipment running smoothly.</p>
-                    <a href="#" class="filled-button">Read More</a>
-                  </div>
-                </div>
-              </div>
-            </div>
-          </div>
-        </div>
-      </div>
-    </div>
-
-    <div class="testimonials">
-      <div class="container">
-        <div class="row">
-          <div class="col-md-12">
-            <div class="section-heading">
-              <h2>What they say <em>about us</em></h2>
-              <span>testimonials from our greatest clients</span>
-            </div>
-          </div>
-          <div class="col-md-12">
-            <div class="owl-testimonials owl-carousel">
-              
-             
-              
-              <div class="testimonial-item">
-                <div class="inner-content">
-                  <h4>David Nuey</h4>
-                  <span>Chief Admin</span>
-                  <p>"They provide on time and reliable repairing servicing in a friendly budget, We are very happy to do business and use their servicesin our hospital"</p>
-                </div>
-                <img src="http://placehold.it/60x60" alt="">
-              </div>
-              
-              <div class="testimonial-item">
-                <div class="inner-content">
-                  <h4>Rashid Saleh</h4>
-                  <span>Techincal Head</span>
-                  <p>"Very professional and dedicated staff, they have deep knowledge and training of high end medical equipment, so we trust them and recommend them"</p>
-                </div>
-                <img src="http://placehold.it/60x60" alt="">
-              </div>
-              
-            </div>
-          </div>
-        </div>
-      </div>
-    </div>
-
-   
-
-
-    <!-- Footer Starts Here -->
-    <footer>
-      <div class="container">
-        <div class="row">
-          <div class="col-md-3 footer-item">
-            <h4>HBEEB. MEDICAL EQUIPMENT REPAIRING COMPANY</h4>
-            <p>Office no G-02, KHALED MOHAMMED ABDULLAH ALZAHED BUILDING,HOR AL ANZ, DUBAI,UAE</p>
-            <ul class="social-icons">
-              <li><a rel="nofollow" href="https://fb.com/templatemo" target="_blank"><i class="fa fa-facebook"></i></a></li>
-              <li><a href="#"><i class="fa fa-twitter"></i></a></li>
-              <li><a href="#"><i class="fa fa-linkedin"></i></a></li>
-              <li><a href="#"><i class="fa fa-behance"></i></a></li>
-            </ul>
-          </div>
-          <div class="col-md-3 footer-item">
-            <h4>Useful Links</h4>
-            <ul class="menu-list">
-             <li><a href="#">About Us</a></li>
-              <li><a href="#">How We Work</a></li>
-              <li><a href="#">Quick Support</a></li>
-              <li><a href="#">Contact Us</a></li>
-              <li><a href="#">Privacy Policy</a></li>
-            </ul>
-          </div>
-          <div class="col-md-3 footer-item">
-            <h4>Additional Pages</h4>
-            <ul class="menu-list">
-              <li><a href="#">About Us</a></li>
-              <li><a href="#">How We Work</a></li>
-              <li><a href="#">Quick Support</a></li>
-              <li><a href="#">Contact Us</a></li>
-              <li><a href="#">Privacy Policy</a></li>
-            </ul>
-          </div>
-          <div class="col-md-3 footer-item last-item">
-            <h4>Contact Us</h4>
-            <div class="contact-form">
-              <form id="contact footer-contact" action="" method="post">
-                <div class="row">
-                  <div class="col-lg-12 col-md-12 col-sm-12">
-                    <fieldset>
-                      <input name="name" type="text" class="form-control" id="name" placeholder="Full Name" required="">
-                    </fieldset>
-                  </div>
-                  <div class="col-lg-12 col-md-12 col-sm-12">
-                    <fieldset>
-                      <input name="email" type="text" class="form-control" id="email" pattern="[^ @]*@[^ @]*" placeholder="E-Mail Address" required="">
-                    </fieldset>
-                  </div>
-                  <div class="col-lg-12">
-                    <fieldset>
-                      <textarea name="message" rows="6" class="form-control" id="message" placeholder="Your Message" required=""></textarea>
-                    </fieldset>
-                  </div>
-                  <div class="col-lg-12">
-                    <fieldset>
-                      <button type="submit" id="form-submit" class="filled-button">Send Message</button>
-                    </fieldset>
-                  </div>
-                </div>
-              </form>
-            </div>
-          </div>
-        </div>
-      </div>
-    </footer>
-    
-    <div class="sub-footer">
-      <div class="container">
-        <div class="row">
-          <div class="col-md-12">
-            <p>Copyright &copy; 2025 HBEEB. MEDICAL EQUIPMENT REPAIRING COMPANY.
-            
-            - Design: <a rel="nofollow noopener" href="http://www.card.ueuo.com" target="_blank">CaRD</a></p>
-          </div>
-        </div>
-      </div>
-    </div>
-
-    <!-- Bootstrap core JavaScript -->
-    <script src="vendor/jquery/jquery.min.js"></script>
-    <script src="vendor/bootstrap/js/bootstrap.bundle.min.js"></script>
-
-    <!-- Additional Scripts -->
-    <script src="assets/js/custom.js"></script>
-    <script src="assets/js/owl.js"></script>
-    <script src="assets/js/slick.js"></script>
-    <script src="assets/js/accordions.js"></script>
-
-    <script language = "text/Javascript"> 
-      cleared[0] = cleared[1] = cleared[2] = 0; //set a cleared flag for each field
-      function clearField(t){                   //declaring the array outside of the
-      if(! cleared[t.id]){                      // function makes it static and global
-          cleared[t.id] = 1;  // you could use true and false, but that's more typing
-          t.value='';         // with more chance of typos
-          t.style.color='#fff';
-          }
-      }
-    </script>
-
-  </body>
-</html>
+$kernel->terminate($request, $response);
